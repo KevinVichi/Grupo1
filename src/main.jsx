f948fcf7eddf2334f847fed8528f64873ef8722d
KevinVichi <vichicelakevin@gmail.com>
2024-11-19 09:40:33 -0500

    Cambios index

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-/*Leer la etiqueta root del <div id="root"></div> 
-y monta la aplicacion de react en ese div*/
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   //Archivo principal de la app de react
   <React.StrictMode>
     {/*Aplicacion montada*/}
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
